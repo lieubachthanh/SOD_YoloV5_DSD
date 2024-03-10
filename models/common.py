9b89dc0b5b88de70c5c8151a2bd00c9e77cc099a
Liêu Bách Thành <lieubachthanh2002@gmail.com>
2024-03-10 22:50:44 +0700

    update

--- models/common.py
+++ models/common.py
@@ -278,44 +278,92 @@ class C3_Res2(nn.Module):
         self.cv1 = Conv(c1, c_, 1, 1)
         self.cv2 = Conv(c1, c_, 1, 1)
         self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
-        self.m = nn.Sequential(*(Bottleneck_Res2(c_, c_, shortcut, g, e=1.0, res2_scale=res2_scale) for _ in range(n)))
+        self.m = nn.Sequential(*(Res2NetBottleneck(c_, c_, shortcut, g, e=1.0, res2_scale=res2_scale) for _ in range(n)))
 
     def forward(self, x):
         return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
 
-class Bottleneck_Res2(nn.Module):
-    # Bottleneck with Res2Net module
-    def __init__(self, c1, c2, shortcut=True, g=1, e=0.5, res2_scale=1.0):  # ch_in, ch_out, shortcut, groups, expansion, res2_scale
-        super().__init__()
-        c_ = int(c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, c_, 1, 1)
-        self.cv2 = nn.Sequential(
-            Res2Net_Module(c_, c_, res2_scale),
-            Conv(c_, c2, 3, 1, g=g)
-        )
-        self.add = shortcut and c1 == c2
+class Res2NetBottleneck(nn.Module):
+    expansion = 4
 
-class Res2Net_Module(nn.Module):
-    # Res2Net module from Gao et al. 'Res2Net: A New Multi-scale Backbone Architecture'
-    def __init__(self, c1, c2, res2_scale=1.0):
-        super().__init__()
-        self.split = nn.Sequential(
-            Conv(c1, c1 * res2_scale, 1, 1),
-            nn.Identity(),
-            nn.Identity()
-        )
-        self.channels = [int(c2 * res2_scale // 2)] * 2
-        self.conv = nn.ModuleList([Conv(c1 * res2_scale // 2, c2 // 2, 3, 1, 1) for _ in range(2)])
-        self.concat = Conv(c2, c2, 1, 1)
+    def __init__(self, inplanes, planes, downsample=None, stride=1, scales=4, groups=1, se=False,  norm_layer=None):
+        super(Res2NetBottleneck, self).__init__()
+        if planes % scales != 0:
+            raise ValueError('Planes must be divisible by scales')
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        bottleneck_planes = groups * planes
+        self.conv1 = conv1x1(inplanes, bottleneck_planes, stride)
+        self.bn1 = norm_layer(bottleneck_planes)
+        self.conv2 = nn.ModuleList([conv3x3(bottleneck_planes // scales, bottleneck_planes // scales, groups=groups) for _ in range(scales-1)])
+        self.bn2 = nn.ModuleList([norm_layer(bottleneck_planes // scales) for _ in range(scales-1)])
+        self.conv3 = conv1x1(bottleneck_planes, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.se = SEModule(planes * self.expansion) if se else None
+        self.downsample = downsample
+        self.stride = stride
+        self.scales = scales
 
     def forward(self, x):
-        x1 = self.split[1](x)
-        x2 = self.split[2](x)
-        x1 = self.conv[0](x1)
-        x2 = self.conv[1](x2)
-        x = torch.cat((x1, x2), 1)
-        x = self.concat(x)
-        return x
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        xs = torch.chunk(out, self.scales, 1)
+        ys = []
+        for s in range(self.scales):
+            if s == 0:
+                ys.append(xs[s])
+            elif s == 1:
+                ys.append(self.relu(self.bn2[s-1](self.conv2[s-1](xs[s]))))
+            else:
+                ys.append(self.relu(self.bn2[s-1](self.conv2[s-1](xs[s] + ys[-1]))))
+        out = torch.cat(ys, 1)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.se is not None:
+            out = self.se(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, groups=groups, bias=False)
+
+
+def conv1x1(in_planes, out_planes, stride=1):
+    """1x1 convolution"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+
+
+class SEModule(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(SEModule, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Conv2d(channels, channels // reduction, kernel_size=1, padding=0)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Conv2d(channels // reduction, channels, kernel_size=1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input):
+        x = self.avg_pool(input)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return input * x
 
 #--------------------------------------------------------------------------------------------------
 class C3x(C3):
